f2666f2ebb5ab180606872804f3baa03ef8c4d22
Alex Clark <alexclark4121@gmail.com>
2019-03-27 19:19:41 -0400

    added comments

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+
+//function that makes the skills bar moved based on percentage // 
+
 jQuery(document).ready(function(){
 	jQuery('.skillbar').each(function(){
 		jQuery(this).find('.skillbar-bar').animate({
